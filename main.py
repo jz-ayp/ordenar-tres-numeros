342eafc830663ed7c97ccad18371f4d49752d1a1
Jorge Zaldívar <jorge.zaldivar@gmail.com>
2021-02-27 10:26:44 -0600

    Update main.py
    Delete solution

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 Inserta el encabezado aquí y escribe tu código abajo
-
+"""
 
 # Declaraciones
 CONSTANTE = valor
@@ -13,37 +13,3 @@ entrada = input()
 
 # Salidas
 print(salida)
-"""
-
-"""
-Ordenar tres números de mayor a menor 
-"""
-
-# Entradas
-a =	eval(input("Introduzca el primer número: "))
-b = eval(input("Introduzca el segundo número: "))
-c = eval(input("Introduzca el tercer número: "))
-	
-# Proceso
-# Primero se acomodan los dos primeros 
-# y luego se coloca el tercero en su lugar
-if a >= b:
-	mayor = a
-	menor = b
-else:
-	mayor = b
-	menor = a
-
-# Ahora acomodar c en su lugar
-if c >= mayor:
-	medio = mayor
-	mayor = c
-elif c <= menor:
-	medio = menor
-	menor = c
-else:
-	medio = c
-
-# Salidas
-print("Los números en orden de mayor a menor son:",)
-print(mayor, medio, menor)
