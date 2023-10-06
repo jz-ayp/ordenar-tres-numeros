d6ac7329347ebb1e0c6b8432f3d1968bb85d3d48
Jorge Zaldívar <jorge.zaldivar@gmail.com>
2023-10-05 20:52:03 -0600

    Remove solution

--- ordenar3.py
+++ ordenar3.py
@@ -1,32 +1,15 @@
 """
-Ordenar tres números de mayor a menor 
+Inserta el encabezado aquí y escribe tu código abajo
 """
 
-# Entradas
-a =	float(input("Introduzca el primer número: "))
-b = float(input("Introduzca el segundo número: "))
-c = float(input("Introduzca el tercer número: "))
-	
-# Proceso
-# Primero se acomodan los dos primeros 
-# y luego se coloca el tercero en su lugar
-if a >= b:
-	mayor = a
-	menor = b
-else:
-	mayor = b
-	menor = a
+# Declaraciones
+CONSTANTE = valor
 
-# Ahora acomodar c en su lugar
-if c >= mayor:
-	medio = mayor
-	mayor = c
-elif c <= menor:
-	medio = menor
-	menor = c
-else:
-	medio = c
+# Entradas
+entrada = input()
+
+# Proceso
+
 
 # Salidas
-print("Los números en orden de mayor a menor son:",)
-print(mayor, medio, menor)
+print(salida)
